a9070b386e1bac92e9efb7f2ee4adfcfab1e6ad4
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-25 12:45:59 -0500

    Initial Commit

---
+++ ScrollViewImageGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageGallery
+//
+//  Created by Kerry Toonen on 2016-01-25.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewImageGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewImageGallery
+//
+//  Created by Kerry Toonen on 2016-01-25.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
